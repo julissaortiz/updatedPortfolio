e5cfc38a94f0285ff16dffbb22cbc1746dd9790a
julissaortiz <126351385+julissaortiz@users.noreply.github.com>
2023-08-16 23:35:47 -0500

    updated files and started on app.js

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+import profpic from "../assets/images/prof.png";
+
+function About() {
+  return (
+    <div className="flex items-center">
+      <div className="ml-14">
+        <h1 className="text-3xl font-bold text-purple mb-4">About Me</h1>
+        <p className="text-lg">
+          Welcome ! I am a full stack webx alumn from the University of
+          Minnesota Bootcamp ! I have been working on a few projects and I'm
+          excited to be able to showcase them for you. I am very creative and
+          have an open mind, I hope you have enjoyed these projects, and hope to
+          be in contact soon !
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
